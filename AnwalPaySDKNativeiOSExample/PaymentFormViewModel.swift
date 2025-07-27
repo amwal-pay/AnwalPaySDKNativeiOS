ebcbbc6a29ebd19fb549b6d984a90f21554ffcfb
Amr Saied <amr.elskaan@amwal-pay.com>
2025-07-28 01:17:11 +0300

    Fix

--- AnwalPaySDKNativeiOSExample/PaymentFormViewModel.swift
+++ AnwalPaySDKNativeiOSExample/PaymentFormViewModel.swift
@@ -11,13 +11,13 @@ import amwalsdk
 // ViewModel class conforming to ObservableObject
 class PaymentFormViewModel: ObservableObject {    
     
-    @Published var merchantId: String = "73092"
-    @Published var terminalId: String = "861788"
+    @Published var merchantId: String = "7394"
+    @Published var terminalId: String = "196456"
     @Published var amount: String = "1"
     @Published var currency: Config.Currency = .OMR
     @Published var language: Config.Locale = .en
     @Published var transactionType: TransactionType = .CARD_WALLET
-    @Published var secureHash: String = "21C4060616C3E1F221EB1FF83184C70BA77E6AB2F204C3339A64FC902072CFCE"
+    @Published var secureHash: String = "C0873776E2290E208FBBA27795DFC51B1531E73D187518106FE45DF344865149"
     @Published var selectedEnv: Config.Environment = .SIT
     @Published var customerId: String?
 }
